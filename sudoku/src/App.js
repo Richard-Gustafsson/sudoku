0cdd869138722e7d99e44e1ca31bdf71bffed89e
Richard Gustafsson <rille117@hotmail.com>
2018-03-11 15:18:24 +0100

    started working...

--- sudoku/src/App.js
+++ sudoku/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SudokuBoard from './components/sudokuBoard';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>HEJ</p>
+        <p>SUDOKU</p>
+        <SudokuBoard />
       </div>
     );
   }
